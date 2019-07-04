7af97dd057774c132b4e8dcebe04d5b0bd17716a
gabdulkhanov <gabdulkhanov.lenar@mail.ru>
2019-07-04 20:55:44 +0500

    Переделал через jQuery

--- script.js
+++ script.js
@@ -1,49 +1,120 @@
 window.onload = function() {
-	var sp = document.getElementsByClassName("sp");
-	var tr1 = document.getElementsByClassName("tr1");
-	var tr2 = document.getElementsByClassName("tr2");
 
-for (let i = 0; i < sp.length; i++)
-{
-	sp[i].onmouseover = function() {		
-		hover("#e85607", i);
+	var cert = document.getElementsByClassName("cert");
+	var cert_overlay = document.getElementsByClassName("cert_overlay");
+	var scale = document.getElementsByClassName("scale");
+
+	var openModal = $("#openModal");
+	var forma = $("#forma");
+	var close = $(".close");
+
+$('.btn').click(function(){	
+   	$('.btn').not(this).removeClass('active');
+    $(this).toggleClass('active');
+});
+
+/**********hover-эффекты******/
+/***certificat***/	
+
+	$(".cert").css("position", "relative");
+	$(".cert").css("cursor", "pointer");
+	$(".cert_overlay").css("position", "absolute");
+	$(".scale").css("position", "absolute");
+
+for (let i = 0; i < cert.length; i++) {
+		cert[i].onmouseover = function() {	
+			scale[i].style.display = "block";		
+			scale[i].style.background = 'url("imgs/certificates/scale.png") center no-repeat';
+			scale[i].style.width = "52px";
+			scale[i].style.height = "53px";
+			scale[i].style.zIndex = "1";
+			scale[i].style.margin = "150px 0 0 100px";
+
+			cert_overlay[i].style.display = "block";		
+			cert_overlay[i].style.background = 'url("imgs/certificates/Rectangle.png") center no-repeat';
+			cert_overlay[i].style.width = "100%";
+			cert_overlay[i].style.height = "350px";
+			cert_overlay[i].style.zIndex = "0";				
+		}
+
+		cert[i].onmouseout = function() {	
+				cert_overlay[i].style.display = "none";	
+				scale[i].style.display = "none";	
+		}
+
+		cert[i].onclick = function() {
+			openModal.css( "display", "block" );
+			openModal.css( "pointerEvents", "auto" );
+		}
+	}	
+
+
+	close[0].onclick = function() {		
+		openModal.css( "display", "none" );
+		openModal.css( "pointerEvents", "none" );
+	}
+	close[1].onclick = function() {		
+		forma.css( "display", "none" );
+		forma.css( "pointerEvents", "none" );
+	}	
+
+
+/**********buttons******/
+
+$(".sp").mouseover(function() {	
+	hover("#e85607");
+});
+$(".sp").mouseout(function() {	
+	hover("#17594A");
+});
+
+$(".tr1").mouseover(function() {	
+	hover("#e85607");
+});
+$(".tr1").mouseout(function() {	
+	hover("#17594A");
+});
+
+$(".tr2").mouseover(function() {	
+	hover("#e85607");
+});
+$(".tr2").mouseout(function() {	
+	hover("#17594A");
+});
+
+$(".sp").click(function() {	
+		display();
+	});
+
+$(".tr1").click(function() {			
+		display();
+	});
+
+$(".tr2").click(function() {			
+		display();
+});
+
+
+	function display() {
+		forma.css( "display", "block" );
+		forma.css( "pointerEvents", "auto" );
 	}
 
-	sp[i].onmouseout = function() {	
-		hover("#17594A", i);		
+	function hover(clr) {
+		$(".sp").css("cursor","pointer");
+		$(".tr1").css("cursor","pointer");
+		$(".tr2").css("cursor","pointer");		
+
+		$(".sp").css("backgroundColor", clr);
+		$(".tr1").css("width", "0");
+		$(".tr1").css("height", "0");
+		$(".tr1").css("borderBottom", "40px solid " + clr);
+		$(".tr1").css("borderLeft", "40px solid transparent");
+
+		$(".tr2").css("width", "0");
+		$(".tr2").css("height", "0");
+		$(".tr2").css("borderBottom", "40px solid transparent");
+		$(".tr2").css("borderLeft", "40px solid " + clr);
 	}
 
-	tr1[i].onmouseover = function() {		
-		hover("#e85607", i);
-	}
-
-	tr1[i].onmouseout = function() {	
-		hover("#17594A", i);		
-	}
-
-	tr2[i].onmouseover = function() {		
-		hover("#e85607", i);
-	}
-
-	tr2[i].onmouseout = function() {	
-		hover("#17594A", i);		
-	}
-}
-
-	function hover(clr, ind) {
-		sp[ind].style.cursor = "pointer";
-		tr1[ind].style.cursor = "pointer";
-		tr2[ind].style.cursor = "pointer";
-
-		sp[ind].style.backgroundColor = clr;
-		tr1[ind].style.width = 0;
-		tr1[ind].style.height = 0;
-		tr1[ind].style.borderBottom = "40px solid " + clr;
-		tr1[ind].style.borderLeft = "40px solid transparent";
-
-		tr2[ind].style.width = 0;
-		tr2[ind].style.height = 0;
-		tr2[ind].style.borderBottom = "40px solid transparent";
-		tr2[ind].style.borderLeft = "40px solid " + clr;
-	}
 }
